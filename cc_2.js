70e89c8760d4a87f23180fb965df269fda49bdf9
Brandon Mejia <brandonmejiabusiness@gmail.com>
2024-09-08 12:42:34 -0400

    Create and test CalculateTip function

--- cc_2.js
+++ cc_2.js
@@ -6,3 +6,11 @@ let tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
 //Task 2 - Output Details
 
 console.log('The was bill $${bill}, the tip was $${tip}, and the total value is $${bill + tip}');
+
+//Task 3 - Create a Function
+
+function calculateTup(bill) {
+    return bill >= 50 && bill <=300 ? bill * 0.15 : bill * 0.20;
+}
+let testbill = 100
+console.log(calculateTip(testBill));
